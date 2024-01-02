362629cc8ad075c1acd756f0c31bc597dd61261f
mazghul <mazghul@gmail.com>
2024-01-02 23:43:44 +0400

    Adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'error',
+    'no-extra-bind': 'error',
+    'no-implicit-globals': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
+/* eslint-env node */
+
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+  }
+}
